31982950598049596ac6aea090995691aa64d101
松島一徳 <>
2018-08-07 21:59:16 +0800

    スタジオ詳細画面のモック作成（予約ページへのリンクボタンが未完成）

--- detail.php
+++ detail.php
@@ -29,6 +29,54 @@
     </ul>
   </nav>
 
+  <div class="container main-body detail-box">
+    <div class="row">
+      <div class="col-md-4 col-xs-12">
+        <img src="img/IMG_2105.jpg" class="studio-img">
+      </div>
+      <div class="col-md-8 col-xs-12">
+        <h1>新宿マイスタジオ</h1>
+        <h2>Aルーム</h2>
+      </div>
+      <div class="col-md-12">
+        <table class="table table-striped detail-info">
+          <caption>スタジオの詳細情報</caption>
+            <tr>
+              <td>スタジオの広さ</td>
+              <td>〇〇㎡</td>
+            </tr>
+            <tr>
+              <td>目安人数</td>
+              <td>20人</td>
+            </tr>
+            <tr>
+              <td>最寄駅</td>
+              <td>新宿駅</td>
+            </tr>
+            <tr>
+              <td>電話番号</td>
+              <td>03-0000-0000</td>
+            </tr>
+            <tr>
+              <td>住所</td>
+              <td>東京都新宿区西新宿〇〇</td>
+            </tr>
+        </table>
+      </div>
+      <div class="col-md-12">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.2870185148395!2d139.69594231509814!3d35.694553980191195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd63a036d8b%3A0xc711fed0636fd85b!2z5paw5a6_44Oe44Kk44K544K_44K444Kq!5e0!3m2!1sen!2sph!4v1533637111654" width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+     
+      <div class="d-block d-md-none col-sm-6 col-xs-6" style="background-color: red;">
+        <p>スタジオに電話する</p>
+      </div>
+      <div class="col-sm-12 col-xs-6" style="background-color: blue;">
+        <p>このスタジオを予約する</p>
+      </div>
+    </div>
+  </div>
+
+  
   <footer>
     <a href="#">プライバシーポリシー</a>
     <p>&copy; 2018 -Studio NAVI -</p>
