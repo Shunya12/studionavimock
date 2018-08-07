c707a312e97581e6a8c14b5901c9919753c1ac4f
松島一徳 <>
2018-08-07 15:37:56 +0800

    トップページと検索ページを作成

---
+++ result.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta discription="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <a class="navbar-brand mr-auto navbar-brand-center" href="#">Studio NAVI</a>
+  <ul class="navbar-nav">
+    <li class="nav-item d-none d-sm-block ">
+      <a class="nav-link" href="#">新規登録</a>
+    </li>
+    <li class="nav-item d-none d-sm-block">
+      <a class="nav-link" href="#">ログイン</a>
+    </li>
+    <li class="nav-item d-none d-sm-block">
+      <a class="nav-link" href="#">ログアウト</a>
+    </li>
+    <li class="nav-item d-none d-sm-block">
+      <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
+    </li>
+  </ul>
+</nav>
+ <section class="main-body">
+    <div class="card-deck">
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+
+    </div>
+      <div class="card-deck">
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      
+    </div>
+    <div class="card-deck">
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="img/IMG_2105.jpg">
+        <div class="card-body">
+          <h5 class="card-title">〇〇スタジオ Aスタジオ</h5>
+          <p class="card-text">テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト テキスト </p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">このスタジオの詳細を見る</small>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+
+
+  <footer>
+    <a href="#">プライバシーポリシー</a>
+    <p>&copy; 2018 -Studio NAVI -</p>
+  </footer>
+
+</body>
+</html>
---
+++ top.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>東京のレンタルスタジオならStudioNAVI</title>
+  <meta charset="utf-8">
+  <meta discription="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+  <a class="navbar-brand mr-auto navbar-brand-center" href="#">Studio NAVI</a>
+  <ul class="navbar-nav">
+    <li class="nav-item d-none d-sm-block ">
+      <a class="nav-link" href="#">新規登録</a>
+    </li>
+    <li class="nav-item d-none d-sm-block">
+      <a class="nav-link" href="#">ログイン</a>
+    </li>
+    <li class="nav-item d-none d-sm-block">
+      <a class="nav-link" href="#">ログアウト</a>
+    </li>
+    <li class="nav-item d-none d-sm-block">
+      <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
+    </li>
+  </ul>
+  </nav>
+
+  <div class="container-fluid main-body">
+    <div class="row">
+      <div class="col-md-4 main-menu">
+        <a href="#">
+          <div class="filter">
+            <img src="img/menu1.jpg">
+            <p>条件から<br>スタジオを探す</p>
+          </div>
+        </a>
+      </div>
+      <div class="col-md-4 main-menu">
+        <a href="#">
+          <div class="filter">
+            <img src="img/menu2.jpg">
+            <p>エリアから<br>スタジオを探す</p>
+          </div>
+        </a>
+      </div>
+      <div class="col-md-4 main-menu">
+        <a href="#">
+          <div class="filter">
+            <img src="img/menu3.jpg">
+            <p>予約確認</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+
+
+  <footer>
+    <a href="#">プライバシーポリシー</a>
+    <p>&copy; 2018 -Studio NAVI -</p>
+  </footer>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
